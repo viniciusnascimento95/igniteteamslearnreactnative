e3e5d45dec13972212183dea6c94a2cbd715e7f4
vinicius fernandes <nascimento.vinicius32@gmail.com>
2024-01-29 23:23:21 -0300

    update css

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import { Groups } from '@screens/Groups';
 
+import { ThemeProvider } from 'styled-components';
+import theme from './src/theme';
 export default function App() {
   return (
+    <ThemeProvider theme={theme}>
     <Groups />
+    </ThemeProvider>
   );
 }
 
