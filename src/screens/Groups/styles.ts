309430f5bb68626f4d8bf61cd79f0d6a973ced9b
vinicius fernandes <nascimento.vinicius32@gmail.com>
2024-01-31 17:24:40 -0300

    config styles default

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -3,11 +3,7 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex: 1;
-    align-items: center;
-    justify-content: center;    
+    background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+    padding: 24px;
 `;
-
-export const Text = styled.Text`
-    color: '#fff';
-    font-size: 32px;
-`;
+ 
