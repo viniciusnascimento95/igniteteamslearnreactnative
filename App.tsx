e32190bbbb0773690fa4486076ec2ee3c636bdc9
Vinicius Fernandes <nascimento.vinicius32@gmail.com>
2024-02-03 16:00:10 -0300

    adicionando imagem no app

--- App.tsx
+++ App.tsx
@@ -3,12 +3,17 @@ import { Groups } from '@screens/Groups';
 import { ThemeProvider } from 'styled-components';
 
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator, StatusBar } from 'react-native';
 import theme from './src/theme';
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
 
       {fontsLoaded ? <Groups /> : <ActivityIndicator />}
 
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,10 @@
-
-import { Text } from 'react-native';
 import { Container } from './styles';
+import { Header } from '@components/Header';
 
 export function Groups() {
   return (
     <Container style={Container}>
-      <Text>Groups!</Text>
+      <Header />
     </Container>
   );
 }
