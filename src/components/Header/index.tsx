7e4278735e3d68a60a6c1bf1faac9d0955b0ae3c
Vinicius Fernandes <nascimento.vinicius32@gmail.com>
2024-02-04 18:32:19 -0300

    criando componente header e highlight

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,17 @@
-import { Container, Logo} from './styles';
+import { Container, Logo, BackIcon, BackButton } from './styles';
+import logoImg from '@assets/logo.png'
 
-import logoImg from '../../../assets/logo.png'
-export function Header(){
-    return(
+type Props = {
+    showBackButton?: boolean
+}
+export function Header({ showBackButton = false }: Props) {
+    return (
         <Container>
-            
+            {showBackButton &&
+                <BackButton>
+                    <BackIcon />
+                </BackButton>
+            }
             <Logo source={logoImg} />
         </Container>
     )
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,13 @@
+import { Highlight } from '@components/Highlight';
 import { Container } from './styles';
 import { Header } from '@components/Header';
 
 export function Groups() {
   return (
     <Container style={Container}>
-      <Header />
+      <Header showBackButton/>
+
+      <Highlight title='Turmas' subtitle='Jogue com a sua turma' />
     </Container>
   );
 }
