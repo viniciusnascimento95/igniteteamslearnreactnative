aea1e4faf9b040e395d6d46fa577d2fd54b327cb
vinicius fernandes <nascimento.vinicius32@gmail.com>
2024-02-28 17:19:12 -0300

    adicionando componente flatlist

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,15 +2,30 @@ import { Highlight } from '@components/Highlight';
 import { Container } from './styles';
 import { Header } from '@components/Header';
 import { GroupCard } from '@components/GroupCard';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 
 export function Groups() {
+
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket'])
+
   return (
     <Container style={Container}>
-      <Header showBackButton/> 
+      <Header showBackButton />
 
       <Highlight title='Turmas' subtitle='Jogue com a sua turma' />
 
-      <GroupCard title='Galera do Ignite' />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item} />
+        )
+        }
+
+      />
+
     </Container>
   );
 }
