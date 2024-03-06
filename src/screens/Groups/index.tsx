4ab62137c2740a0d56fbda26084f6d38ff20b09e
vinicius fernandes <nascimento.vinicius32@gmail.com>
2024-03-05 21:49:40 -0300

    criando componente buttão

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { GroupCard } from '@components/GroupCard';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Groups() {
 
@@ -27,6 +28,11 @@ export function Groups() {
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma ? " />}
 
+      />
+
+      <Button
+        title='Criar nova turma'
+        // type='SECONDARY'
       />
 
     </Container>
