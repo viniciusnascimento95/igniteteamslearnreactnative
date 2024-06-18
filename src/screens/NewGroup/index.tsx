bc2f50f03531808f9d7e5a6ba5826f9547dfeef5
vinicius fernandes <nascimento.vinicius32@gmail.com>
2024-06-17 23:13:00 -0300

    add: screen newGroup

---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,20 @@
+import { Highlight } from '@components/Highlight';
+import { Container } from './styles';
+import { Header } from '@components/Header';
+import { GroupCard } from '@components/GroupCard';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
+
+export function NewGroups() {
+
+  return (
+    <Container style={Container}>
+     
+
+    </Container>
+  );
+}
+
+
