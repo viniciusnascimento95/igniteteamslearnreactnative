6e28f8d17c9773cb82c07a358e44f0c76884cd89
Vinicius Fernandes <nascimento.vinicius32@gmail.com>
2024-07-15 23:31:53 -0300

    add config styles placeholder in component input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,13 +1,15 @@
 import { TextInputProps } from "react-native";
 import { Container } from "./style";
+import { useTheme } from "styled-components/native";
 
 
 export function Input({ ...rest }: TextInputProps) {
 
+  const { COLORS } = useTheme()
+
   return (
     <Container
-
-
+      placeholderTextColor={COLORS.GRAY_300}
       {...rest}
     />
   )
