eb73164b734c4268e10f53908228077a0e829098
Vinicius Fernandes <nascimento.vinicius32@gmail.com>
2024-07-16 23:24:05 -0300

    add: config helps and styles updated

--- src/components/GroupCard/styles.ts
+++ src/components/GroupCard/styles.ts
@@ -1,31 +1,32 @@
 import { UsersThree } from "phosphor-react-native";
 import { TouchableOpacity } from "react-native";
-import styled from "styled-components/native";
+
+import styled, { css } from "styled-components/native";
 
 export const Container = styled(TouchableOpacity)`
     width: 100%;
     height: 90px;
-
-    background-color:  ${({ theme }) => theme.COLORS.GRAY_500};
+    background-color: ${({ theme }) => theme.COLORS.GRAY_500};
     border-radius: 6px;
-
     flex-direction: row;
     align-items: center;
-
     padding: 24px;
     padding-bottom: 12px;
 `;
 
 export const Title = styled.Text`
-    font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-    color :  ${({ theme }) => theme.COLORS.GRAY_200};
-    font-family:  ${({ theme }) => theme.FONT_FAMILY.REGULAR};;
-    `;
+
+${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.MD}px;
+    color: ${theme.COLORS.GRAY_200};
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    `}
+`;
 
 export const Icon = styled(UsersThree).attrs(({ theme }) => ({
     size: 32,
     color: theme.COLORS.GREEN_700,
-    weight : 'fill'
+    weight: 'fill'
 }))`
-    margin-right : 20px;
-    `;
+    margin-right: 20px;
+`;
--- src/components/Input/style.ts
+++ src/components/Input/style.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { TextInput } from 'react-native'
 
 export const Container = styled(TextInput)`
@@ -6,13 +6,13 @@ export const Container = styled(TextInput)`
 
     min-height:  56px;
     max-height: 56px;
-    background-color: ${({ theme }) => theme.COLORS.GRAY_700};
-    color: ${({ theme }) => theme.COLORS.WHITE};
-    font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
-    font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-   
+
+    ${({ theme }) => css`
+        background-color: ${theme.COLORS.GRAY_700};
+        color: ${theme.COLORS.WHITE};
+        font-family: ${theme.FONT_FAMILY.REGULAR};
+        font-size: ${theme.FONT_SIZE.MD}px;
+    `}   
     border-radius: 6px;
     padding: 16px;
-
-
 `;
